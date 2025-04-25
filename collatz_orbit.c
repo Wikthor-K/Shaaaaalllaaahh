f14f590ef6a35b1756095f09c8d15b1b83599d9f
Wikthor-K <wiktor.kern@outlook.com>
2025-04-25 16:59:50 +0300

    Update collatz_orbit.c
    test

--- collatz_orbit.c
+++ collatz_orbit.c
@@ -1,5 +1,21 @@
 #include <stdint.h>
 
 uint64_t collatz_orbit(uint64_t n, uint64_t k) {
-    // Implement the function here
+    if (n==0) {
+        return 0;
+    }
+
+    uint64_t i;
+    for (i=0; i<k; i++) {
+        if (n%2==0) {
+            n = n/2;
+        } 
+        else {
+            if (n > (UINT64_MAX - 1)/3) {
+                return 0;
+            }
+            n = 3*n + 1;
+        }
+    }
+    return n;
 }
